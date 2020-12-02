c4365ac5e76a581e9e838d2b5e19eae7ce0c371b
Rakeshpodaralla1 <72673707+Rakeshpodaralla1@users.noreply.github.com>
2020-12-02 19:10:12 +0530

    commit app.js

---
+++ app.js
@@ -0,0 +1,11 @@
+//name of the mentor must declare variable with var
+var mentor = "Rakesh";
+console.log(mentor);
+
+//state of the person must declare variable with let
+let state = "Texas";
+console.log(state);
+
+//Hair color of the person must declare variable with const
+const haircolor = "Brown";
+console.log(haircolor);
